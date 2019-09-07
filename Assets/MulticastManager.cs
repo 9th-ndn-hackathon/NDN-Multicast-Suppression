a3e7b7430bd721016237df6948a67e2e2f5aa8ca
Ernest McCracken <ernestm@us.ibm.com>
2019-09-07 14:24:09 -0400

    Added NFDConsumer changes

---
+++ Assets/MulticastManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulticastManager : MonoBehaviour
+{
+    public float interestGenerationRate;
+    public int interestGenerationCount;
+
+    private static MulticastManager instance;
+
+    public static MulticastManager getInstanceOf()
+    {
+        return instance;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
